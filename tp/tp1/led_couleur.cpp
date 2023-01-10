d4da7f954dc436c2166488c6996416b3f7dfed6b
Laurent Bourgon <laurent.bourgon@polymtl.ca>
2023-01-10 15:47:17 -0500

    hey it works

--- tp/tp1/led_couleur.cpp
+++ tp/tp1/led_couleur.cpp
@@ -2,13 +2,15 @@
  * Nom: compteur 32 bits
  * Copyright (C) 2005 Matthew Khouzam
  * License http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * Description: Ceci est un exemple simple de programme 
+ * Description: Ceci est un exemple simple de programme
  * Version: 1.1
  */
 
-
-#include <avr/io.h> 
+#include <avr/io.h>
 #include <util/delay_basic.h>
+#include <util/delay.h>
+
+#define F_CPU 8000000
 
 int main()
 {
@@ -16,12 +18,30 @@ int main()
   DDRB = 0xff; // PORT B est en mode sortie
   DDRC = 0xff; // PORT C est en mode sortie
   DDRD = 0xff; // PORT D est en mode sortie
-    
-  for(;;)  // boucle sans fin
-  {
-    _delay_ms(99);
-    
-  }
-  return 0; 
-}
 
+  unsigned long compteur = 0;
+
+  uint8_t ETEINT = 0x00;
+  uint8_t VERT = 0x01;
+  uint8_t ROUGE = 0x02;
+
+  for (;;) // boucle sans fin
+  {
+    PORTA = ROUGE;
+
+    _delay_ms(5000);
+
+    PORTA = VERT;
+
+    _delay_ms(5000);
+
+    for (int i = 0; i < 2500; i++)
+    {
+      PORTA = VERT;
+      _delay_ms(1);
+      PORTA = ROUGE;
+      _delay_ms(1);
+    }
+  }
+  return 0;
+}
