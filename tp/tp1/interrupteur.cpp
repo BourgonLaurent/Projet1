f3f4832fba6c889643d17d1e352a609a669f3625
Laurent Bourgon <laurent.bourgon@polymtl.ca>
2023-01-12 09:55:02 -0500

    interrupteur working

---
+++ tp/tp1/interrupteur.cpp
@@ -0,0 +1,63 @@
+/*
+ * Nom: interrupteur
+ * Description: Affichage séquentiel de la diode
+ */
+
+#define F_CPU 8000000
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+// couleurs possibles de la diode bicolore
+enum Color : uint8_t
+{
+    Off = 0x00,
+    Green = 0x01,
+    Red = 0x02
+};
+
+void showColor(Color color)
+{
+    PORTA = color;
+}
+
+void showOff()
+{
+    showColor(Color::Off);
+}
+
+void showRed()
+{
+    showColor(Color::Red);
+}
+
+bool isButtonPressed()
+{
+    return PIND & 0b0000'0010;
+}
+
+int main()
+{
+    // ne met en sortie que les broches A1 et A2
+    DDRA = 0b00000011;
+
+    // séquence de la diode:
+    //  - rouge
+    //  - vert
+    //  - ambré (rouge + vert varie rapidement)
+    while (true)
+    {
+        if (isButtonPressed())
+        {
+            _delay_ms(10);
+            if (isButtonPressed())
+            {
+                showRed();
+                continue;
+            }
+        }
+        showOff();
+    }
+
+    return 0;
+}
