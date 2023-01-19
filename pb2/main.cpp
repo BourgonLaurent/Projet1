d6079ec03ea685c87b20ee4f7966e77e47f06834
Laurent Bourgon <laurent.bourgon@polymtl.ca>
2023-01-19 13:36:10 -0500

    [pb2] rough state table

--- pb2/main.cpp
+++ pb2/main.cpp
@@ -1,6 +1,6 @@
 /**
  * Problem 2:
- *  Press three times the switch button to turn the LED green.
+ *  Cycles through LED colors based on a state table.
  *
  * \author Mehdi Benouhoud
  * \author Laurent Bourgon
@@ -12,23 +12,33 @@
  *
  * Implements the following state table:
  *
- * +---------------+----+--------------+-------+
- * | Current State | D2 |  Next State  |   A   |
- * +===============+====+==============+=======+
- * |               |  0 |     INIT     |  OFF  |
- * |      INIT     +----+--------------+-------+
- * |               |  1 |  FIRST_PRESS |  OFF  |
- * +---------------+----+--------------+-------+
- * |               |  0 |  FIRST_PRESS |  OFF  |
- * |  FIRST_PRESS  +----+--------------+-------+
- * |               |  1 | SECOND_PRESS |  OFF  |
- * +---------------+----+--------------+-------+
- * |               |  0 | SECOND_PRESS |  OFF  |
- * |  SECOND_PRESS +----+--------------+-------+
- * |               |  1 |  THIRD_PRESS |  OFF  |
- * +---------------+----+--------------+-------+
- * |  THIRD_PRESS  |  x |     INIT     | GREEN |
- * +---------------+----+--------------+-------+
+ * +-----------------+----+----------------+-------+
+ * |  Current State  | D2 |Next State      |   A   |
+ * +=================+====+================+=======+
+ * |                 |  0 |INIT            |       |
+ * |       INIT      +----+----------------+  RED  +
+ * |                 |  1 |FIRST_PRESS     |       |
+ * +-----------------+----+----------------+-------+
+ * |                 |  0 |FIRST_RELEASE   |       |
+ * |   FIRST_PRESS   +----+----------------+ AMBER +
+ * |                 |  1 |FIRST_PRESS     |       |
+ * +-----------------+----+----------------+-------+
+ * |                 |  0 |FIRST_RELEASE   |       |
+ * |  FIRST_RELEASE  +----+----------------+ GREEN +
+ * |                 |  1 |SECOND_PRESS    |       |
+ * +-----------------+----+----------------+-------+
+ * |                 |  0 |SECOND_RELEASE  |       |
+ * |   SECOND_PRESS  +----+----------------+  RED  +
+ * |                 |  1 |SECOND_PRESS    |       |
+ * +-----------------+----+----------------+-------+
+ * |                 |  0 |SECOND_RELEASE  |       |
+ * |  SECOND_RELEASE +----+----------------+  OFF  +
+ * |                 |  1 |THIRD_PRESS     |       |
+ * +-----------------+----+----------------+-------+
+ * |                 |  0 |INIT            |       |
+ * |   THIRD_PRESS   +----+----------------+ GREEN +
+ * |                 |  1 |SECOND_PRESS    |       |
+ * +-----------------+----+----------------+-------+
  */
 
 #define F_CPU 8000000 // CPU clock frequency, used by <util/delay.h>
@@ -42,12 +52,12 @@ constexpr int COLOR_DELAY_MS = 2000;
 
 enum class MachineState
 {
-    INIT_RED,
-    AMBER,
-    FIRST_GREEN,
-    SECOND_RED,
-    FIRST_OFF,
-    SECOND_GREEN
+    INIT,
+    FIRST_PRESS,
+    FIRST_RELEASE,
+    SECOND_PRESS,
+    SECOND_RELEASE,
+    THIRD_PRESS
 };
 
 int main()
@@ -55,61 +65,61 @@ int main()
     DDRA |= _BV(DDA0) | _BV(DDA1);
     DDRD &= ~_BV(DDD2);
 
-    MachineState currentState = MachineState::INIT_RED;
+    MachineState currentState = MachineState::INIT;
 
     while (true)
     {
         switch (currentState)
         {
-        case MachineState::INIT_RED:
+        case MachineState::INIT:
             PORTA = (uint8_t)Color::RED;
 
             if (Button::isPressed())
             {
-                currentState = MachineState::AMBER;
+                currentState = MachineState::FIRST_PRESS;
             }
             break;
 
-        case MachineState::AMBER:
+        case MachineState::FIRST_PRESS:
             PORTA = (uint8_t)Color::RED;
             _delay_ms(10);
             PORTA = (uint8_t)Color::GREEN;
 
             if (!Button::isPressed())
             {
-                currentState = MachineState::FIRST_GREEN;
+                currentState = MachineState::FIRST_RELEASE;
             }
             break;
 
-        case MachineState::FIRST_GREEN:
+        case MachineState::FIRST_RELEASE:
             PORTA = (uint8_t)Color::GREEN;
             if (Button::isPressed())
             {
-                currentState = MachineState::SECOND_RED;
+                currentState = MachineState::SECOND_PRESS;
             }
             break;
 
-        case MachineState::SECOND_RED:
+        case MachineState::SECOND_PRESS:
             PORTA = (uint8_t)Color::RED;
             if (!Button::isPressed())
             {
-                currentState = MachineState::FIRST_OFF;
+                currentState = MachineState::SECOND_RELEASE;
             }
             break;
 
-        case MachineState::FIRST_OFF:
+        case MachineState::SECOND_RELEASE:
             PORTA = (uint8_t)Color::OFF;
             if (Button::isPressed())
             {
-                currentState = MachineState::SECOND_GREEN;
+                currentState = MachineState::THIRD_PRESS;
             }
             break;
 
-        case MachineState::SECOND_GREEN:
+        case MachineState::THIRD_PRESS:
             PORTA = (uint8_t)Color::GREEN;
             if (!Button::isPressed())
             {
-                currentState = MachineState::INIT_RED;
+                currentState = MachineState::INIT;
             }
             break;
         }
