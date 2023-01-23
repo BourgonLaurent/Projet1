fc458a479fc9872ad5658b17f605d0ab2ccea77f
Laurent Bourgon <laurent.bourgon@polymtl.ca>
2023-01-23 16:45:22 -0500

    [tp2] add LED component

---
+++ tp/tp2/components/led.hpp
@@ -0,0 +1,33 @@
+/**
+ * Implementation of the control of an LED.
+ *
+ * \author Mehdi Benouhoud
+ * \author Laurent Bourgon
+ * \date January 19, 2023
+ */
+
+#ifndef LED_H
+#define LED_H
+
+#include <avr/io.h>
+
+#include <tp2/components/colors.hpp>
+
+class LED
+{
+public:
+    LED(
+        volatile uint8_t &dataDirectionRegister,
+        volatile uint8_t &port,
+        const uint8_t plusBit,
+        const uint8_t minusBit);
+
+    void setColor(const Color &color);
+
+private:
+    volatile uint8_t &port;
+    const uint8_t plusBit;
+    const uint8_t minusBit;
+};
+
+#endif
