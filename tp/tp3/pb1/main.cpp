ee4450546062422e0a7ac5ce2c7bd6ce224d4145
Laurent Bourgon <laurent.bourgon@polymtl.ca>
2023-01-26 10:34:07 -0500

    [TP3] [pb1] found a way w/o magic numbers

--- tp/tp3/pb1/main.cpp
+++ tp/tp3/pb1/main.cpp
@@ -17,27 +17,39 @@
 #include <tp2/components/led.hpp>
 #include <tp2/components/colors.hpp>
 
-void wait(uint16_t delay)
+void wait(double delay)
 {
-    for (uint16_t i = 0; i < delay; i++)
+    if (delay == 0)
+    {
+        return;
+    }
+
+    for (uint16_t i = 0; i < delay / 40; i++)
     {
         _delay_us(10);
     }
 }
 
-constexpr __uint24 TOTAL_SHUTDOWN_US = 3000000;
+constexpr uint32_t TOTAL_SHUTDOWN_US = 3000000;
 constexpr uint16_t PERIOD_US = 1000;
+constexpr uint16_t NUMBER_OF_CYCLES = TOTAL_SHUTDOWN_US / PERIOD_US;
+constexpr double INCREMENT_RATIO = 1.0 / NUMBER_OF_CYCLES;
 
 int main()
 {
-    LED led = LED(&DDRA, &PORTA, PORTA0, PORTA1);
+    LED led = LED(&DDRA, &PORTA, PORTA1, PORTA0);
 
-    for (uint16_t i = 0; i < PERIOD_US / 2; i += TOTAL_SHUTDOWN_US / PERIOD_US / PERIOD_US)
+    double a = 1;
+    double c = 0;
+    for (uint16_t i = 0; i < NUMBER_OF_CYCLES; i++)
     {
         led.setColor(Color::GREEN);
-        wait(PERIOD_US - i);
+        wait(PERIOD_US * a);
         led.setColor(Color::OFF);
-        wait(i);
+        wait(PERIOD_US * c);
+
+        a -= INCREMENT_RATIO;
+        c += INCREMENT_RATIO;
     }
 
     return 0;
