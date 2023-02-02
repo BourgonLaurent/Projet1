ef51dc9798989fd3ed0020ad7074e5a514c94561
Laurent Bourgon <laurent.bourgon@polymtl.ca>
2023-02-02 11:44:44 -0500

    [tp4] [pb2] wait for user after 10s

--- tp/tp4/pb2/main.cpp
+++ tp/tp4/pb2/main.cpp
@@ -1,83 +1,100 @@
 #define F_CPU 8000000UL
-#include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
 #include <util/delay.h>
 
 #include <tp2/components/button.hpp>
-#include <tp2/components/led.hpp>
 #include <tp2/components/colors.hpp>
+#include <tp2/components/led.hpp>
 
 constexpr uint8_t DEBOUNCE_DELAY_MS = 10;
-constexpr uint16_t TIMESPAN_MS = 10'000;
 constexpr uint8_t FLASH_DURATION_MS = 100;
 
-volatile uint8_t gExpiredTimespan;
-volatile uint8_t gButton;
+constexpr uint16_t CYCLES_PER_SECOND = F_CPU / 1024;
+constexpr uint8_t FLASHING_DELAY_S = 1;
+constexpr uint16_t FLASHING_CYCLES = CYCLES_PER_SECOND * FLASHING_DELAY_S;
+
+constexpr uint16_t WAIT_DURATION_MS = 10000;
+
+volatile bool buttonWasPressed = false;
+volatile bool waitForUser = false;
+
+LED led = LED(&DDRA, &PORTA, PORTA0, PORTA1);
+
+void clearInterrupts()
+{
+    EIFR |= _BV(INTF0);
+}
 
 ISR(TIMER1_COMPA_vect)
 {
-    gExpiredTimespan = 1;
-    _delay_ms(FLASH_DURATION_MS);
-    EIFR |= _BV(INTF0);
+    if (!waitForUser) {
+        led.setColor(Color::RED);
+        _delay_ms(100);
+        led.setColor(Color::OFF);
+    }
+
+    clearInterrupts();
 }
 
-ISR(TIMER1_COMPB_vect)
+ISR(INT0_vect)
 {
-    gButton = 1;
-    // Anti-bounce
-    // TODO implement anti-bounce
-    _delay_ms(DEBOUNCE_DELAY_MS);
-    EIFR |= _BV(INTF0);
+    ::buttonWasPressed = 1;
+
+    clearInterrupts();
 }
 
-void elapseTimespan(uint16_t duration)
+void elapseTimespan()
 {
-    gExpiredTimespan = 0;
-
     // mode CTC du timer 1 avec horloge divisée par 1024 (p. 130)
-    TCCR1B |= _BV(WGM13) | _BV(WGM12);
-
-    TCCR1B |= _BV(CS10) | _BV(CS12);
-    TCCR1B &= ~_BV(CS11);
-    // interruption après la durée spécifiée
-
-    TCNT1 = 0;
-
-    OCR1A = duration;
-
-    // page 130
+    TCCR1B |= _BV(WGM12);
+    TCCR1B &= ~_BV(WGM13);
     TCCR1A &= ~(_BV(WGM11) | _BV(WGM10));
 
-    TCCR1C = 0;
+    // prescale at 1024
+    TCCR1B |= _BV(CS10) | _BV(CS12);
+    TCCR1B &= ~_BV(CS11);
 
-    TIMSK1 = _BV(OCIE1A);
+    // interruption après la durée spécifiée
+    TCNT1 = 0; // counter
+    TCCR1C = 0;
+    TIMSK1 |= _BV(OCIE1A);
+
+    OCR1A = FLASHING_CYCLES; // Output Compare Register
+}
+
+void prepareInterrupts()
+{
+    // (p.68) allow interruptions
+    EIMSK |= _BV(INT0);
+
+    // (p.67) falling + rising edge
+    EICRA |= _BV(ISC00);
+    EICRA &= ~_BV(ISC01);
 }
 
 int main()
 {
-    LED led = LED(&DDRA, &PORTA, PORTA0, PORTA1);
+    DDRD &= ~_BV(PD2);
 
-    do
-    {
+    cli();
+    prepareInterrupts();
+    elapseTimespan();
+    sei();
+
+    _delay_ms(WAIT_DURATION_MS);
+    ::waitForUser = true;
+
+    do {
         led.setColor(Color::OFF);
-        elapseTimespan(TIMESPAN_MS);
-
-        // attendre qu'une des deux variables soit modifiée
-
-        // par une ou l'autre des interruptions.
-
-    } while (gExpiredTimespan == 0 && gButton == 0);
+    } while (::buttonWasPressed == 0);
 
     // Une interruption s'est produite. Arrêter toute
-
-    led.setColor(Color::RED);
     // forme d'interruption. Une seule réponse suffit.
-
     cli();
 
     // Verifier la réponse
-
-    // 'modifier ici'
+    led.setColor(Color::RED);
 
     EICRA |= _BV(ISC00) | _BV(ISC01);
     return 0;
