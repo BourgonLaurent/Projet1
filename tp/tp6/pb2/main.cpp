32d6d3c7e3cb6cb70e71cf413f15c6d7bb180d05
Mehdi Benouhoud <mehdi.benouhoud7@gmail.com>
2023-02-16 11:22:20 -0500

    [tp6] [pb2] done (needs cleaning)

---
+++ tp/tp6/pb2/main.cpp
@@ -0,0 +1,40 @@
+/**
+ * Problem 2:
+ *  Blablablou
+ *
+ * \author Mehdi Benouhoud
+ * \author Laurent Bourgon
+ * \date February 16, 2023
+ *
+ */
+
+#define F_CPU 8000000UL
+#include <util/delay.h>
+
+#include <tp2/components/led.hpp>
+#include <tp6/components/can.h>
+
+int main()
+{
+    LED led = LED(&DDRB, &PORTB, PORTB1, PORTB0);
+    can analogDigitalConverter = can();
+
+    while (true) {
+        uint8_t readValue = analogDigitalConverter.lecture(PA0) >> 2;
+
+        if (readValue <= 170) {
+            led.setColor(Color::GREEN);
+        }
+        else if (readValue >= 235) {
+            led.setColor(Color::RED);
+        }
+        else {
+            led.setColor(Color::GREEN);
+            _delay_ms(10);
+            led.setColor(Color::RED);
+            _delay_ms(10);
+        }
+    }
+
+    return 0;
+};
