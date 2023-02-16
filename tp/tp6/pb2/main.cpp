8755331b25326cd1ffc95b8c47870c1f3b806818
Mehdi Benouhoud <mehdi.benouhoud7@gmail.com>
2023-02-16 11:33:14 -0500

    [tp6] [pb2] cleaner code

--- tp/tp6/pb2/main.cpp
+++ tp/tp6/pb2/main.cpp
@@ -15,7 +15,8 @@
 #include <tp6/components/can.h>
 
 constexpr uint8_t THRESHOLD_LOW_LIGHT = 170;
-// constexpr uint8_t
+constexpr uint8_t THRESHOLD_HIGH_LIGHT = 235;
+constexpr uint8_t AMBER_DELAY_MS = 10;
 
 int main()
 {
@@ -25,17 +26,17 @@ int main()
     while (true) {
         uint8_t readValue = analogDigitalConverter.lecture(PA0) >> 2;
 
-        if (readValue <= 170) {
+        if (readValue <= THRESHOLD_LOW_LIGHT) {
             led.setColor(Color::GREEN);
         }
-        else if (readValue >= 235) {
+        else if (readValue >= THRESHOLD_HIGH_LIGHT) {
             led.setColor(Color::RED);
         }
         else {
             led.setColor(Color::GREEN);
-            _delay_ms(10);
+            _delay_ms(AMBER_DELAY_MS);
             led.setColor(Color::RED);
-            _delay_ms(10);
+            _delay_ms(AMBER_DELAY_MS);
         }
     }
 
