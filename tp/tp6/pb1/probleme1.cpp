9a8806b61230a37be1987209e3bb029ed02bf2f3
Catalina Andrea Araya Figueroa <catalina-andrea.araya-figueroapolymtl.ca>
2023-02-16 12:25:45 -0500

    tp6 avec usart

--- tp/tp6/pb1/probleme1.cpp
+++ tp/tp6/pb1/probleme1.cpp
@@ -4,6 +4,7 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
+#include <memoire_24.h>
 
 const uint8_t LED_GREEN = 1 << PA0;
 const uint8_t LED_RED = 1 << PA1;
@@ -14,6 +15,28 @@ const uint8_t DELAY_COLOR_AMBRER = 5;
 volatile uint8_t gBoutonPoussoir = 0;
 volatile uint8_t change = 0;
 volatile uint8_t compteur = 0;
+
+void initialisationUART(void)
+{
+    UBRR0H = 0;
+    UBRR0L = 0xCF;
+    // permettre la réception et la transmission par le UART0
+    // (1 << U2X0)
+    UCSR0A = (1 << MPCM0) | (1 << UDRE0) | (1 << RXC0);
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+    // Format des trames: 8 bits, 1 stop bits, sans parité
+    UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);
+}
+
+// Du USART vers le PC
+
+void transmissionUART(uint8_t donnee)
+{
+
+    while (!(UCSR0A & (1 << UDRE0)))
+        ;
+    UDR0 = donnee;
+}
 
 bool isPressed()
 {
@@ -162,9 +185,12 @@ ISR(TIMER1_COMPA_vect)
 int main()
 {
     DDRA = 0xff;
+
     initialisation();
+
     while (true)
     {
+        transmissionUART(compteur);
         setNextState();
         setColorLed();
     }
