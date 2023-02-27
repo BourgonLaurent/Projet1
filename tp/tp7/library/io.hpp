41f611f03d9e456aa2e6cb3e587b87cf6d858cb1
Mehdi Benouhoud <mehdi.benouhoud7@gmail.com>
2023-02-27 18:37:39 -0500

    [TP7] Makefile for library

---
+++ tp/tp7/library/io.hpp
@@ -0,0 +1,42 @@
+/**
+ * Bit manipulation.
+ *
+ * Team #45
+ * \author Mehdi Benouhoud
+ * \author Laurent Bourgon
+ * \date January 19, 2023
+ */
+
+#ifndef IO_H
+#define IO_H
+
+#include <avr/io.h>
+
+namespace io {
+    static inline void setActive(volatile uint8_t* port, const uint8_t bit)
+    {
+        *port |= _BV(bit);
+    }
+
+    static inline void clear(volatile uint8_t* port, const uint8_t bit)
+    {
+        *port &= ~_BV(bit);
+    }
+
+    static inline bool get(volatile uint8_t* pin, const uint8_t bit)
+    {
+        return *pin & _BV(bit);
+    }
+
+    static inline void setInput(volatile uint8_t* port, const uint8_t bit)
+    {
+        clear(port, bit);
+    }
+
+    static inline void setOutput(volatile uint8_t* port, const uint8_t bit)
+    {
+        setActive(port, bit);
+    }
+} // namespace io
+
+#endif
