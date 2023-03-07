7ac7ad494921cafb8889ff2fb4fa8ad17551673f
Ihsane03 <113052273+Ihsane03@users.noreply.github.com>
2023-03-07 13:39:27 -0500

    Tp7/6 memory (#19)
    * renamed class + arrange comments
    
    * [TP7][Communication] add #include Communication
    
    * [TP7][Memory] Define Memory methods - change return type Memory::read
    
    * fix comments + add Memory::read method with no return
    
    * [TP7][Memory] add methods to read/write a charater
    
    * [TP7][Memory] fixed return type Memory::read
    
    * [lib] [memory] remove files not related to PR
    
    * [lib] [memory] inherit Memoire24CXXX
    
    * [lib] [memory] add limitations warning
    
    * [lib] [memory] move delay to cpp
    
    * [lib] [memory] add const + rename Memoire24CXXX file
    
    * [lib] [memory] remove delay when reading
    
    ---------
    
    Co-authored-by: Laurent Bourgon <laurent.bourgon@polymtl.ca>

---
+++ tp/tp7/lib/memory.cpp
@@ -0,0 +1,54 @@
+/**
+ * Write and read external EEPROM memory.
+ *
+ * LIMITATIONS: Memory::readMessage is using a shared buffer.
+ *              Copy or use the value read before reading another one.
+ *
+ * Team #4546
+ * \author Catalina Andrea Araya Figueroa
+ * \author Mehdi Benouhoud
+ * \author Laurent Bourgon
+ * \author Ihsane Majdoubi
+ *
+ * \date March 1, 2023
+ */
+
+#include <string.h>
+#include <util/delay.h>
+
+#include "memory.hpp"
+
+const uint8_t Memory::read(const uint16_t address) const
+{
+    uint8_t readCharacter;
+    lecture(address, &readCharacter);
+
+    return readCharacter;
+}
+
+void Memory::write(const uint16_t address, const uint8_t data)
+{
+    ecriture(address, data);
+    _delay_ms(READ_WRITE_DELAY_MS);
+}
+
+const char* Memory::readMessage(const uint16_t startAddress,
+                                const uint8_t messageSize) const
+{
+    lecture(startAddress, (uint8_t*)readMessageBuffer_, messageSize);
+
+    return readMessageBuffer_;
+}
+
+void Memory::writeMessage(const uint16_t startAddress, const char* message)
+{
+    const uint8_t messageSize = strlen(message) + 1;
+
+    ecriture(startAddress, (uint8_t*)message, messageSize);
+    _delay_ms(READ_WRITE_DELAY_MS);
+}
+
+void Memory::clearBuffer()
+{
+    memset(readMessageBuffer_, 0, MAXIMUM_MESSAGE_SIZE);
+}
---
+++ tp/tp7/lib/memory.hpp
@@ -0,0 +1,41 @@
+/**
+ * Write and read external EEPROM memory.
+ *
+ * LIMITATIONS: Memory::readMessage is using a shared buffer.
+ *              Copy or use the value read before reading another one.
+ *
+ *
+ * Team #4546
+ * \author Catalina Andrea Araya Figueroa
+ * \author Mehdi Benouhoud
+ * \author Laurent Bourgon
+ * \author Ihsane Majdoubi
+ *
+ * \date March 1, 2023
+ */
+
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <lib/_memoire_24.hpp>
+
+class Memory : private Memoire24CXXX
+{
+public:
+    void write(const uint16_t address, const uint8_t data);
+    const uint8_t read(const uint16_t address) const;
+
+    void writeMessage(const uint16_t startAddress, const char* message);
+    const char* readMessage(const uint16_t startAddress,
+                            const uint8_t messageSize) const;
+
+    void clearBuffer();
+
+private:
+    static constexpr uint8_t READ_WRITE_DELAY_MS = 5;
+    static constexpr uint8_t MAXIMUM_MESSAGE_SIZE = 127;
+
+    char readMessageBuffer_[MAXIMUM_MESSAGE_SIZE];
+};
+
+#endif
