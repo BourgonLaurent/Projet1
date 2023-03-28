4f49be6222933b5efd124b26d40f5c23e247b77b
Ihsane Majdoubi <ihsane.majdoubi@polymtl.ca>
2023-03-28 14:10:19 -0400

    [DETECT] test: read value from IrSensor

---
+++ projet/tests/main.cpp
@@ -0,0 +1,34 @@
+/**
+ * Read value from IrSensor
+ *
+ *
+ * Team #4546
+ *  \author Catalina Andrea Araya Figueroa
+ *  \author Mehdi Benouhoud
+ *  \author Laurent Bourgon
+ *  \author Ihsane Majdoubi
+ *
+ * \date March 28, 2023
+ */
+
+#include <avr/io.h>
+#include <lib1900/analogReader.hpp>
+#include <lib1900/communication.hpp>
+#include <lib1900/debug.hpp>
+#include <lib1900/interruptTimer.hpp>
+#include <lib1900/interrupts.hpp>
+#include <lib1900/irSensor.hpp>
+#include <lib1900/led.hpp>
+#include <lib1900/wheels.hpp>
+#include <util/delay.h>
+
+constexpr io::Position SENSOR = PA6;
+
+int main()
+{
+    IrSensor irSensor = IrSensor(SENSOR);
+    Communication::initialize();
+
+    uint8_t value = irSensor.read();
+    Communication::send(value);
+}
