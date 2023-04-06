363c1da47bcd56409a286d507ff5c5c855a59c2f
Laurent Bourgon <laurent.bourgon@polymtl.ca>
2023-04-06 17:28:15 -0400

    add header

--- projet/lib/flasher.cpp
+++ projet/lib/flasher.cpp
@@ -1,3 +1,42 @@
+/**
+ * Flash a LED at a given frequency.
+ *
+ * LIMITATIONS:
+ *  Since Flasher modifies the configuration
+ *  of InterruptTimer, they cannot be used at
+ *  the same time.
+ *
+ * Hardware Identification
+ *  TIMER: Uses InterruptTimer (Timer 1).
+ *
+ * USAGE: Use `Flasher_vect` (or `InterruptTimer_vect`) for the ISR.
+ *    ISR(Flasher_vect)
+ *    {
+ *        Flasher::handleFlash();
+ *        ...
+ *    }
+ *
+ *   Led led(&PORTB, &DDRB, PB0, PB1);
+ *
+ *   // Flash the LED on and off at 2Hz.
+ *   Flasher::initialize(led, 2, Led::Color::RED, Led::Color::OFF);
+ *   Flasher::startFlashing();
+ *   Flasher::stopFlashing();
+ *
+ *   // Show the LED amber.
+ *   Flasher::initializeAmber(led);
+ *   Flasher::startFlashing();
+ *   Flasher::stopFlashing();
+ *
+ * Team #4546
+ *  \author Catalina Andrea Araya Figueroa
+ *  \author Mehdi Benouhoud
+ *  \author Laurent Bourgon
+ *  \author Ihsane Majdoubi
+ *
+ * \date April 5, 2023
+ */
+
 #include "flasher.hpp"
 
 #include <lib/interruptTimer.hpp>
