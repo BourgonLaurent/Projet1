c4855741b7bcc468348724255c465e76734f4e11
Ihsane Majdoubi <ihsane.majdoubi@polymtl.ca>
2023-04-19 19:03:50 -0400

    change delay

--- projet/app/detect/constants.hpp
+++ projet/app/detect/constants.hpp
@@ -20,13 +20,13 @@
 
 namespace constants {
     // Used by detect
-    static constexpr double DELAY_TURN_90_MS = 2.0;
+    static constexpr double DELAY_TURN_90_MS = 2.5;
 
     // Constants unique to the robot
     // Used by ObjectFinder
     // le robot vérifie si le poteau est devant lui après avoir tourné
-    static constexpr double FIRST_DELAY_IS_IN_FRONT_MS = 1.1;
-    static constexpr double SECOND_DELAY_IS_IN_FRONT_MS = 1; // 1.75
+    static constexpr double FIRST_DELAY_IS_IN_FRONT_MS = 0.8;
+    static constexpr double SECOND_DELAY_IS_IN_FRONT_MS = 1.0; // 1.75
 
     // le robot cherche le poteau après l'avoir perdu de vue
     static constexpr double FIRST_DELAY_IN_FRONT_PARK_MS = 1.5; // 1.5
@@ -46,7 +46,7 @@ namespace constants {
 
     static constexpr uint8_t DELAY_TURN_MIDDLE_OBJECT_MS = 100;
     static constexpr uint16_t DELAY_TURNOFF_MS = 125;
-    static constexpr double DELAY_INCREMENT_FIND_LOOP = 0.5;
+    static constexpr double DELAY_INCREMENT_FIND_LOOP = 0.1;
     static constexpr uint16_t DELAY_BETWEEN_FINDS_MS = 1500;
     static constexpr uint16_t DELAY_AFTER_FIND_MS = 1000;
 
